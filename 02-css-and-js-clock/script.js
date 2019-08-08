355ec8ccce4fac303a4f076b8948e05d8e3686e0
davymartinez <davy.martinez@gmail.com>
2019-08-07 21:59:30 -0300

    Add parentheses to .getSeconds(), getMinutes(), and get Hours()

--- 02-css-and-js-clock/script.js
+++ 02-css-and-js-clock/script.js
@@ -6,17 +6,15 @@ const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
   const now = new Date();
-  const seconds = now.getSeconds;
-  const minutes = now.getMinutes;
-  const hours = now.getHours;
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
   const secondsToDegrees = ((seconds / 60) * 360) + 90;
   const minutesToDegrees = ((minutes / 60) * 360) + 90;
   const hoursToDegrees = ((hours / 12) * 360) + 90;
-  $(document).ready(function() {
-    secondHand.style.transform = `rotate(${secondsToDegrees}deg)`;
-    minHand.style.transform = `rotate(${minutesToDegrees}deg)`;
-    hourHand.style.transform = `rotate(${hoursToDegrees})`;
-  });
+  secondHand.style.transform = `rotate(${secondsToDegrees}deg)`;
+  minHand.style.transform = `rotate(${minutesToDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hoursToDegrees})`;
 }
 
 setInterval(setDate, 1000);
