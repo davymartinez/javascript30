ae57762e9cc13f0fc3e5b803754bf75ec1c5de0c
David Martínez <davy.martinez@gmail.com>
2020-11-28 21:23:10 -0300

    Modify styles on JS Drumkit and add comments on JS Clock

--- 02-css-and-js-clock/script.js
+++ 02-css-and-js-clock/script.js
@@ -6,12 +6,19 @@ const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
   const now = new Date();
+  // console.log(`Now: ${now}`);
   const seconds = now.getSeconds();
+  // console.log(`Seconds: ${seconds}`);
   const minutes = now.getMinutes();
+  // console.log(`Minutes: ${minutes}`);
   const hours = now.getHours();
+  // console.log(`Hours: ${hours}`);
   const secondsToDegrees = ((seconds / 60) * 360) + 90;
+  // console.log(`Seconds to degrees: ${secondsToDegrees}`);
   const minutesToDegrees = ((minutes / 60) * 360) + 90;
+  // console.log(`Minutes to degrees: ${minutesToDegrees}`);
   const hoursToDegrees = ((hours / 12) * 360) + 90;
+  // console.log(`Hours to degrees: ${hoursToDegrees}`);
   secondHand.style.transform = `rotate(${secondsToDegrees}deg)`;
   minHand.style.transform = `rotate(${minutesToDegrees}deg)`;
   hourHand.style.transform = `rotate(${hoursToDegrees})`;
