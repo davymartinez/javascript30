0fb989dd820f168afb215476554b41f6460c9d0b
David Martínez <davy.martinez@gmail.com>
2020-11-29 19:10:16 -0300

    Add new control to change background color as well as additional comments on code

--- 03-css-variables-and-js/script.js
+++ 03-css-variables-and-js/script.js
@@ -1,13 +1,17 @@
 /* eslint-disable no-console */
+
+// select all input elements on the page with "controls" class
 const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
+  // this const targets the 'data-sizing="px"' attribute of the spacing and blur controls
+  // (the base color control does not use px, thus the '')
   const suffix = this.dataset.sizing || '';
-  document.documentElement.style.setProperty(
-    `--${this.name}`,
-    this.value + suffix
-  );
+  // set the value of every control as specified in the CSS :root element and HTML name attribute
+  // the value gets the corresponding suffix appended if needed (spacing and blur) 
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
+// call the handleUpdate function on 'change' and 'mousemove' events
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
